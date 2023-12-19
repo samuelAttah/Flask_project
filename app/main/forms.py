fbe622b634b4c97a074776f9129bda823e57756b
Samuel Attah <sammy.attah16@gmail.com>
2023-12-19 21:30:34 +0100

    another commit

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,8 +4,9 @@ from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label="username", validators=[DataRequired()])
-    email_address = StringField(label="email", validators=[DataRequired()])
-    password1 = PasswordField(label="password1")
-    password2 = PasswordField(label="password2")
+    username = StringField(label="User Name:", validators=[DataRequired()])
+    email_address = StringField(
+        label="Email Address: ", validators=[DataRequired()])
+    password1 = PasswordField(label="Password:")
+    password2 = PasswordField(label="Confirm Password:")
     submit = SubmitField('Submit')
